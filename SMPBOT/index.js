08a91b61c1dce27e053a9bafe15cb6f5e3d57216
RhysyPiece360 <67196752+RhysyPiece360@users.noreply.github.com>
2020-11-02 11:59:21 -0500

    Add files via upload

---
+++ SMPBOT/index.js
@@ -0,0 +1,46 @@
+
+const {messageEmbed} = require('discord.js');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setPresence({ activity: { name: 'Happy Day' }, status: 'idle' })
+});
+
+client.on('message', msg => {
+    if (msg.content === '!ping') {
+      msg.channel.send('Pong!');
+    }
+  });
+  
+client.on('message', msg => {
+    if (msg.content === '!help') {
+      msg.channel.send('Commands are \n help \n ping \n ip \n info');
+    }
+  });
+  client.on('message', msg => {
+    if (msg.content === '!ip') {
+      msg.channel.send('Ip:	51.79.33.168:25566');
+    }
+  });
+  client.on('message', message => {
+    if (message.content === '!info') {
+      const embed = new MessageEmbed()
+        .setTitle('INFO')
+        .setColor(0xff0000)
+        .setDescription('')
+        .setfooter('Hanks smp - made by MrFluffy#4697');
+      message.channel.send(embed);
+    }
+  });
+  client.on('guildMemberAdd', member => {
+    const channel = member.guild.channels.cache.find(ch => ch.name === 'sys-messages');
+    if (!channel) return;
+  
+    channel.send(`Welcome to the server, ${member}! we now have ${guild.member}`)
+    channel.send('', {file: ["welcome.jpg"]});
+  });
+  
+ 
+client.login('NzcyMTM2MDA0NTY3MzAyMjI0.X52RsQ.yVAx7UlTHwB6FUWbScAJkjlgQew')
